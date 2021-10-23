9a34233e7573bf4b1ce15cdb2ceffaf4f4ec4a73
facusoto <81945511+facusoto@users.noreply.github.com>
2021-10-23 15:43:48 -0300

    Primer commit de la rama, creado el primer metodo POST en el proyecto, la clase es Models

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
 # importamos el framework
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 # Generamos una variable instanciando el framework
 app = FastAPI()
+
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
 
 # Iniciamos el decorador en el path home
 # Path operation decorator
@@ -11,3 +26,12 @@ app = FastAPI()
 def home():
     # Y retornamos el valor que deseamos
     return {"Hello": "Gaby"}
+
+# Request and Response Body
+
+# Decorador con función post y su path
+@app.post("/person/new")
+# Para retornar los datos hacemos que el parametro
+# sea de tipo la clase que generamos anteriormente
+def create_person(person: Person = Body(...)):
+    return person
