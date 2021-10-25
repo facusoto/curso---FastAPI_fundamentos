2e15c2db2e257dc71286bb842c262f727e5f6ca6
facusoto <81945511+facusoto@users.noreply.github.com>
2021-10-25 15:00:05 -0300

    validation_query

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from typing import Optional
 from pydantic import BaseModel
 
 # importamos el framework
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 
 # Generamos una variable instanciando el framework
 app = FastAPI()
 
 # Models
+
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -30,8 +32,22 @@ def home():
 # Request and Response Body
 
 # Decorador con función post y su path
+
+
 @app.post("/person/new")
 # Para retornar los datos hacemos que el parametro
 # sea de tipo la clase que generamos anteriormente
 def create_person(person: Person = Body(...)):
     return person
+
+# Validaciones: Query paramethers
+
+
+# Lo que el usuario ingresa es un Query paramether
+# Debido a que estos son opcionales se utilizará la libreria
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: Optional[str] = Query(...)
+):
+    return {name: age}
